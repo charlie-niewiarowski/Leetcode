1c2460c9e20203ddf57c450b727923cba6fb2f88
charlie_niew <58190834+charlie-niewiarowski@users.noreply.github.com>
2025-08-17 23:21:41 -0500

    Time: 241 ms (21.77%) | Memory: 18.4 MB (56.85%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,34 +1,17 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        n = len(p)
-        if n > len(s):
-            return []
-            
-        solutionCount = {}
-        for c in p:
-            solutionCount[c] = solutionCount.get(c, 0) + 1
-        
-        winCount, satisfied = {}, 0
-        for i in range(n):
-            winCount[s[i]] = winCount.get(s[i], 0) + 1
-            if s[i] in solutionCount and winCount[s[i]] == solutionCount[s[i]]:
-                satisfied += 1
-        
-        res = []
-        for i in range(len(s) - n):
-            print(satisfied)
-            if satisfied == len(solutionCount):
-                res.append(i)
-        
-            winCount[s[i]] -= 1
-            if s[i] in solutionCount and winCount[s[i]] == solutionCount[s[i]] - 1:
-                satisfied -= 1
+        result = []
+        p_count = Counter(p)
+        window_count = Counter()
 
-            winCount[s[i + n]] = winCount.get(s[i + n], 0) + 1
-            if s[i + n] in solutionCount and winCount[s[i + n]] == solutionCount[s[i + n]]:
-                satisfied += 1
-        
-        if satisfied == len(solutionCount):
-            res.append(len(s) - n)
-        
-        return res
+        for i in range(len(s)):
+            window_count[s[i]] += 1
+
+            if i>=len(p):
+                left =s[i-len(p)]
+                window_count[left] -= 1
+                if window_count[left] == 0:
+                    del window_count[left]
+            if window_count == p_count:
+                result.append(i-len(p)+1)
+        return result
