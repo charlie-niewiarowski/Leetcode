3358b87a24e0bae8882709c10582c7d18526bcff
charlie_niew <58190834+charlie-niewiarowski@users.noreply.github.com>
2025-08-21 21:24:55 -0500

    Time: 46 ms (74.61%) | Memory: 35.6 MB (74.08%) - LeetSync

--- 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.py
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.py
@@ -1,17 +1,12 @@
 class Solution:
     def maximumImportance(self, n: int, roads: List[List[int]]) -> int:
-        degrees = [[0, i] for i in range(n)]
+        degrees = [0] * n
         for u, v in roads:
-            degrees[u][0] += 1
-            degrees[v][0] += 1
+            degrees[u] += 1
+            degrees[v] += 1
         
-        importance = [0] * n
-        degrees.sort()
-        for i, (deg, node) in enumerate(degrees):
-            importance[node] = i + 1
-        
-        res = 0
-        for u, v in roads:
-            res += importance[u] + importance[v]
+        importance, res = 1, 0
+        for deg in sorted(degrees):
+            res += importance * deg
+            importance += 1
         return res
-        
