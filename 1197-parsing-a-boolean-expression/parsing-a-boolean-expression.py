6c549fd87fc00c9437e04b447a261d25888b0f91
charlie_niew <58190834+charlie-niewiarowski@users.noreply.github.com>
2025-09-01 01:10:43 -0500

    Time: 11 ms (82.97%) | Memory: 18.2 MB (11.35%) - LeetSync

---
+++ 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.py
@@ -0,0 +1,37 @@
+class Solution:
+    def parseBoolExpr(self, expression: str) -> bool:
+        s = expression
+        i = 0
+
+        def helper():
+            nonlocal i
+            c = s[i]
+            i += 1
+
+            if c == 't':
+                return True
+            if c == 'f':
+                return False
+            if c == '!':
+                i += 1
+                res = not helper()
+                i += 1
+                return res
+            
+            vals = []
+            i += 1
+            
+            while s[i] != ')':
+                if s[i] != ',':
+                    vals.append(helper())
+                else:
+                    i += 1
+            
+            i += 1
+            if c == '&':
+                return all(vals)
+            else:
+                return any(vals)
+        
+        return helper()
+
