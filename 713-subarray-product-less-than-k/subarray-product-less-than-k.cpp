d1bb8d56b025f58437632116a79d50d45b10f25f
charlie_niew <58190834+charlie-niewiarowski@users.noreply.github.com>
2025-09-13 13:02:07 -0500

    Time: 3 ms (51.29%) | Memory: 65.2 MB (33.09%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
@@ -1,19 +1,23 @@
+#include <vector>
+#include <cmath>
+#include <immintrin.h>
+
 class Solution {
 public:
-    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-        int res = 0, l = 0;
-        int product = 1;
+    int numSubarrayProductLessThanK(const std::vector<int>& nums, int k) {
+        if (k <= 1) return 0;
+        long long res = 0;
+        long long product = 1;
+        int l = 0;
 
-        for (int r = 0; r < nums.size(); ++r) {
+        for (int r = 0; r < (int)nums.size(); ++r) {
             product *= nums[r];
-
             while (l <= r && product >= k) {
                 product /= nums[l++];
             }
-
             res += r - l + 1;
         }
 
-        return res;
+        return (int)res;
     }
 };
