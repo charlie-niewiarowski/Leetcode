4b6dfb2713a24aca693cc3eca265b9f886b36872
charlie_niew <58190834+charlie-niewiarowski@users.noreply.github.com>
2025-09-30 10:45:19 -0500

    Time: 5 ms (6.42%) | Memory: 12.9 MB (18.47%) - LeetSync

---
+++ 867-new-21-game/new-21-game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double new21Game(int n, int k, int maxPts) {
+        if (k == 0) return 1.0;
+
+        vector<double> dp(n + 1, 0.0);
+        dp[0] = 1;
+
+        double sum = 1;
+        for (int r = 1; r <= n; ++r) {
+            dp[r] = sum / maxPts;
+
+            if (r < k) sum += dp[r];
+            if (r - maxPts >= 0 && r - maxPts < k) sum -= dp[r - maxPts];
+        }
+
+        return accumulate(dp.begin() + k, dp.end(), 0.0);
+    }
+};
