8d7981cf90044dfe7c016719deb0e467a4bcfef5
charlie_niew <58190834+charlie-niewiarowski@users.noreply.github.com>
2026-03-03 09:00:44 -0600

    Time: 423 ms (33.94%) | Memory: 211.5 MB (43.19%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,22 +1,17 @@
-/**
-* dp[i][not holding][k] = max(dp[i - 1][not holding][k], dp[i - 1][holding][k - 1] + prices[i])
-* dp[i][holding][k] = max(dp[i - 1][holding][k], dp[i - 1][not holding][k] - prices[i])
-**/
-
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, INT_MIN)));
 
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, INT_MIN)));
         dp[0][0][0] = 0;
         dp[0][1][0] = -1 * prices[0];
-        int res = INT_MIN;
 
+        int res = 0;
         for (int i = 1; i < n; ++i) {
             for (int j = 0; j < 3; ++j) {
                 dp[i][0][j] = dp[i - 1][0][j];
-                if (j > 0 && dp[i - 1][1][j - 1] != INT_MIN) dp[i][0][j] = max(dp[i][0][j], dp[i - 1][1][j - 1] + prices[i]);
+                if (j > 0 && dp[i - 1][1][j - 1] != INT_MIN) dp[i][0][j] = max(dp[i - 1][0][j], dp[i - 1][1][j - 1] + prices[i]);
 
                 dp[i][1][j] = dp[i - 1][1][j];
                 if (dp[i - 1][0][j] != INT_MIN) dp[i][1][j] = max(dp[i][1][j], dp[i - 1][0][j] - prices[i]);
@@ -25,6 +20,6 @@ public:
             }
         }
 
-        return max(0, res);
+        return res;
     }
 };
