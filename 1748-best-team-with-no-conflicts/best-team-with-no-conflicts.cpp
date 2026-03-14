a1c93ba74f52e2f08044681b79f9228f27829dcb
charlie_niew <58190834+charlie-niewiarowski@users.noreply.github.com>
2026-03-14 15:35:36 -0500

    Time: 26 ms (79.74%) | Memory: 24.6 MB (54.20%) - LeetSync

---
+++ 1748-best-team-with-no-conflicts/best-team-with-no-conflicts.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int bestTeamScore(vector<int>& scores, vector<int>& ages) {
+        int n = scores.size();
+
+        vector<pair<int, int>> players;
+        for (int i = 0; i < n; ++i) players.push_back({ages[i], scores[i]});
+        sort(players.begin(), players.end());
+
+        vector<int> dp(n, 0);
+        int res = 0;
+        for (int i = 0; i < n; ++i) {
+            dp[i] = players[i].second;
+            for (int j = 0; j < i; ++j) {
+                if (players[i].second >= players[j].second) {
+                    dp[i] = max(dp[i], dp[j] + players[i].second);
+                }
+            }
+            res = max(res, dp[i]);
+        }
+
+        return res;
+    }
+};
