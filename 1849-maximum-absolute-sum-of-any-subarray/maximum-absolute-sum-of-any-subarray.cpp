554496b937bcbad125e5f9aafd08c659f05587df
charlie_niew <58190834+charlie-niewiarowski@users.noreply.github.com>
2026-03-21 18:52:19 -0500

    Time: 3 ms (17.25%) | Memory: 45.3 MB (23.83%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int maxAbsoluteSum(vector<int>& nums) {
-        int n = nums.size(), res = abs(nums[0]);    
-        vector<pair<int, int>> dp(n);
-        dp[0] = {nums[0], nums[0]};
+        int n = nums.size(), currMax = 0, currMin = 0, res = 0;
 
-        for (int i = 1; i < n; ++i) {
-            dp[i].first = min(dp[i - 1].first + nums[i], nums[i]);
-            dp[i].second = max(dp[i - 1].second + nums[i], nums[i]);
-            res = max(res, max(abs(dp[i].first), dp[i].second));
+        for (int i = 0; i < n; ++i) {
+            currMax = max(0, currMax + nums[i]);
+            currMin = min(0, currMin + nums[i]);
+            res = max(res, max(currMax, abs(currMin)));
         }      
 
         return res;
