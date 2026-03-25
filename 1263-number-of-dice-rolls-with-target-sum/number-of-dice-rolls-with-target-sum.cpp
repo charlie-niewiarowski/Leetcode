5a0a4301775d75e213e58a33c348e5355832e996
charlie_niew <58190834+charlie-niewiarowski@users.noreply.github.com>
2026-03-24 21:06:48 -0500

    Time: 7 ms (90.47%) | Memory: 13.1 MB (23.33%) - LeetSync

---
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -0,0 +1,28 @@
+/*
+    dp[i][j] represents number of ways to have a rolling_sum of j, rolling i die
+    from p = i - k, ..., i - 1, dp[i][j] += dp[i - 1][p];
+    base case p = 1, ..., k, dp[0][p] = 1;
+    return dp[n - 1][target]
+*/  
+
+class Solution {
+public:
+    int numRollsToTarget(int n, int k, int target) {
+        int MOD = 1e9 + 7;
+
+        vector<vector<long long>> dp(n, vector<long long>(target + 1, 0));
+        for (int i = 1; i <= k && i <= target; ++i) dp[0][i] = 1;
+
+        for (int i = 1; i < n; ++i) {
+            if (i > target) break;
+
+            for (int j = i + 1; j <= min(target, (i + 1) * k); ++j) {
+                for (int l = max(i, j - k); l < j; ++l) {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][l]) % MOD;
+                }
+            }
+        }
+
+        return static_cast<int>(dp[n - 1][target] % MOD);
+    }
+};
