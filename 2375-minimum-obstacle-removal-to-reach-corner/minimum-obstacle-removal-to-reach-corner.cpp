0453bfef902374e594c57995bb5158fb80e298ce
charlie_niew <58190834+charlie-niewiarowski@users.noreply.github.com>
2026-08-05 20:42:03 -0400

    Time: 3483 ms (5.01%) | Memory: 278.1 MB (9.14%) - LeetSync

---
+++ 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
@@ -0,0 +1,100 @@
+/*
+we need two queues
+we should only pop the "bad" queue whenever the "good queue" is empty
+each queue will contain r, c, obstacles
+we should have visited[r][c] = best we've seen so far
+let's outsource visitation decisions to a helper function
+
+visited[][]
+*/
+
+class Solution {
+public:
+    int minimumObstacles(vector<vector<int>>& grid) {
+        rows = grid.size();
+        cols = grid[0].size();
+
+        const vector<std::pair<int, int>> directions{
+            {-1, 0}, {1, 0}, {0, -1}, {0, 1}
+        };
+
+        deque<vector<int>> free_queue, blocked_queue;
+        vector<vector<int>> visited(rows, vector<int>(cols, std::numeric_limits<int>::max()));
+
+        free_queue.push_back({0, 0, 0});
+        visited[0][0] = 0;
+
+        int best{std::numeric_limits<int>::max()};
+        while (!free_queue.empty() || !blocked_queue.empty()) {
+            while (!free_queue.empty()) {
+                auto top = free_queue.front();
+                int r = top[0], c = top[1], num_obstacles = top[2];
+                free_queue.pop_front(); 
+
+                if (r == rows - 1 && c == cols - 1) {
+                    best = min(best, num_obstacles);
+                    continue;
+                }
+
+                for (auto [dr, dc] : directions) {
+                    int next_row = r + dr, next_col = c + dc;
+                    if (should_visit(next_row, next_col, num_obstacles, grid, visited)) {
+                        if (grid[next_row][next_col] == 0) {
+                            free_queue.push_back({next_row, next_col, num_obstacles});
+                        }
+                        else {
+                            blocked_queue.push_back({next_row, next_col, num_obstacles + 1});
+                        }
+                    }
+                }
+            }
+
+            while (free_queue.empty() && !blocked_queue.empty()) {
+                auto top = blocked_queue.front();
+                int r = top[0], c = top[1], num_obstacles = top[2];
+                blocked_queue.pop_front(); 
+
+                for (auto [dr, dc] : directions) {
+                    int next_row = r + dr, next_col = c + dc;
+                    if (should_visit(next_row, next_col, num_obstacles, grid, visited)) {
+                        if (grid[next_row][next_col] == 0) {
+                            free_queue.push_back({next_row, next_col, num_obstacles});
+                        }
+                        else {
+                            blocked_queue.push_back({next_row, next_col, num_obstacles + 1});
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                cout << visited[i][j] << " ";
+            }
+            cout << "\n";
+        }
+
+        return best;
+    }
+
+private:
+    int rows, cols;
+
+    bool should_visit(int r, int c, int num_obstacles, const vector<vector<int>>& grid, vector<vector<int>>& visited) {
+        if (r < 0 || r >= rows || c < 0 || c >= cols) {
+            return false;
+        }
+
+        if (grid[r][c] == 1) {
+            ++num_obstacles;
+        }
+
+        if (visited[r][c] > num_obstacles) {
+            visited[r][c] = num_obstacles;
+            return true;
+        }
+
+        return false;
+    }
+};
